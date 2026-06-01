cf102e13a0901ee5d9630042cd97224c1377e9d8
andresutorres <andresutorres@gmail.com>
2026-05-31 20:23:32 -0500

    Fix video loading message in app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 st.title("Video Explicativo sobre el proyecto, Cazadores de trayectorias")
-st.write("En el siguiente video se explica la teoria del tema Tiro Parabólico y el funcionamiento del simulador.
-Nota: El video es pesado, por lo tanto se debe esperar un tiempo prudente para que cargue completamente y se pueda visualizar.")
+st.write("En el siguiente video se explica la teoria del tema Tiro Parabólico y el funcionamiento del simulador.")
+st.write(Nota: El video es pesado, por lo tanto se debe esperar un tiempo prudente para que cargue completamente y se pueda visualizar.")
+
 
 # Cargar y reproducir el video
 st.video("https://www.dropbox.com/scl/fi/e7o0pxh6faqgiu0f5053s/18-30-18.mp4?rlkey=ju0poy01d7llto78ur6zx1e2l&st=rc7jc6vv&raw=1")
