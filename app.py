7157c8db6046840116b120e708033d483a426783
andresutorres <andresutorres@gmail.com>
2026-05-31 20:16:55 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Video Explicativo sobre el proyecto, Cazadores de trayectorias")
+st.write("En el siguiente video se explica la teoria del tema Tiro Parabólico y el funcionamiento del simulador.")
+
+# Cargar y reproducir el video
+st.video("https://www.dropbox.com/scl/fi/e7o0pxh6faqgiu0f5053s/18-30-18.mp4?rlkey=ju0poy01d7llto78ur6zx1e2l&st=rc7jc6vv&raw=1")
